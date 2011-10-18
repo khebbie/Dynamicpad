8ce39aad24457f511f78a927b53053ddc32fb70f
Klaus Hebsgaard <khebsgaard@ebay.com>
2011-10-18 21:30:43 +0200

    cfgSerialiser working

--- DynamicPad/ConnectionStringRepository.cs
+++ DynamicPad/ConnectionStringRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using DynamicPadTests;
 
 namespace DynamicPad
@@ -26,9 +27,9 @@ namespace DynamicPad
         }
     }
 
-    public class CfgFileParser
+    public class CfgFileSerializer
     {
-        public Dictionary<string, string> Parse(string fileContent)
+        public Dictionary<string, string> Serialize(string fileContent)
         {
             var result = new Dictionary<string, string>();
             if(string.IsNullOrWhiteSpace(fileContent))
@@ -42,6 +43,18 @@ namespace DynamicPad
                 result.Add(strings[0].Trim(), strings[1].Trim());
             }
             return result;
+        }
+
+        public string Deserialize(Dictionary<string, string> cfg)
+        {
+            var result =  new StringBuilder();
+            foreach (var cfgLine in cfg)
+            {
+                var line = string.Format("{0}, {1}", cfgLine.Key, cfgLine.Value);
+                result.Append(line);
+                result.Append("\n");
+            }
+            return result.ToString();
         }
     }
 }
---
+++ DynamicPadTests/CfgFileSerializerTests.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using DynamicPad;
+using NUnit.Framework;
+
+namespace DynamicPadTests
+{
+    [TestFixture]
+    public class CfgFileSerializerTests
+    {
+        private CfgFileSerializer _sut;
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            _sut = new CfgFileSerializer();
+        }
+
+        [Test]
+        public void Serialize_Null_ReturnsEmptyDictionary()
+        {
+            Dictionary<string, string> cfg = _sut.Serialize(null);
+
+            Assert.AreEqual(0, cfg.Count);
+        }
+
+        [Test]
+        public void Serialize_NoComma_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => _sut.Serialize("test"));
+        }
+
+        [Test]
+        public void Serialize_TwoStringsSeperatedByComma_DictionaryWithStrings()
+        {
+            const string name = "name";
+            const string connectionstring = "connectionstring";
+            
+            var result = _sut.Serialize( string.Format("{0}, {1}", name, connectionstring));
+
+            var cfg = result.First();
+
+            Assert.AreEqual(name, cfg.Key);
+            Assert.AreEqual(connectionstring, cfg.Value);
+        }
+
+        [Test]
+        public void Deserialize_EmptyDictionary_EmptyString()
+        {
+            var cfg = new Dictionary<string, string>();
+            string result = _sut.Deserialize(cfg);
+            Assert.AreEqual(string.Empty, result);
+        }
+
+        [Test]
+        public void Deserialize_OneEntryDictionary_OneLineCommaSeperated()
+        {
+            const string name = "name";
+            const string connectionstring = "connectionstring";
+            var cfg = new Dictionary<string, string> {{name, connectionstring}};
+            string result = _sut.Deserialize(cfg);
+
+            string expectedResult = string.Format("{0}, {1}\n", name, connectionstring);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void Deserialize_ThreeEntiesDictionary_ThreeLinesCommaSeperated()
+        {
+            const string name = "name";
+            const string connectionstring = "connectionstring";
+            var cfg = new Dictionary<string, string>
+                          {
+                              {name+"1", connectionstring},
+                              {name+"2", connectionstring},
+                              {name+"3", connectionstring}
+                          };
+            string result = _sut.Deserialize(cfg);
+
+            var expectedResult = string.Format("{0}1, {1}", name, connectionstring) + "\n" + string.Format("{0}2, {1}", name, connectionstring) + "\n" + string.Format("{0}3, {1}\n", name, connectionstring);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
