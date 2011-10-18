806dbf9508b15f061f8f02bd3534c149210c3d39
Klaus Hebsgaard <khebsgaard@ebay.com>
2011-10-18 12:10:38 +0200

    fiddling with tests and BadimageFormatException

---
+++ DynamicPadTests/ConnectionStringRepositoryTests.cs
@@ -0,0 +1,18 @@
+﻿using DynamicPad;
+using NUnit.Framework;
+
+namespace DynamicPadTests
+{
+    [TestFixture]
+    public class ConnectionStringRepositoryTests
+    {
+        [Test]
+        public void Add_WhenConnectionStringNameIsNull_ThrowsArgumentNullException()
+        {
+            var sut = new ConnectionStringRepository();
+            sut.Add(null, "");
+
+
+        }
+    }
+}
